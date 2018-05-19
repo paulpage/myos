202d9ea9a4c3f93b2917a4ae66cec0befc450973
Paul Page <pjpage98@gmail.com>
2018-05-19 14:42:35 -0500

    Add qemu run scripts and starting gdt work

--- kernel.cpp
+++ kernel.cpp
@@ -1,14 +1,29 @@
-void printf(char* str)
+#include "types.h"
+#include "gdt.h"
+
+void print(char* str)
+
 {
-    unsigned short* VideoMemory = (unsigned short*)0xb8000;
+    static uint16_t* VideoMemory = (unsigned uint16_t*)0xb8000;
 
     for (int i = 0; str[i] != '\0'; i++)
         VideoMemory[i] = (VideoMemory[i] & 0xFF00) | str[i];
 }
 
-extern "C" void kernelMain(void* multiboot_structure, unsigned int magicnumber)
+typedef void (*constructor)();
+extern "C" constructor start_ctors;
+extern "C" constructor end_ctors;
+extern "C" void callConstructors()
 {
-    printf("Hello, World!");
+    for(constructor* i = &start_ctors; i != &end_ctors; i++)
+        (*i)();
+}
+
+extern "C" void kernelMain(void* multiboot_structure, uint32_t magicnumber)
+{
+    print("Hello, World! This is Paul's OS");
+
+    GlobalDescriptorTable gdt;
 
     while(1);
 }
